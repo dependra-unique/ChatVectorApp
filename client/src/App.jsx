19306ac61ce5a02d0be0b88a134b19d54e2690c4
Dependra Kumar <dependragitcreator@gmail.com>
2024-06-28 17:54:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React, {lazy} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//lazy is used for dynamic import the component one by one
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Chat = lazy(() => import('./pages/Chat'))
+const Groups = lazy(() => import('./pages/Groups'))
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/chat' element={<Chat />}/>
+        <Route path='/groups' element={<Groups />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
